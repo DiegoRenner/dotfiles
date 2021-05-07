42f8ad85aee357d48b917f82cd0f19526e3fec45
drenner <renner@cscs.ch>
2021-05-07 23:32:31 +0200

    extended install package list

--- .scripts/package_list.sh
+++ .scripts/package_list.sh
@@ -1,1 +1,46 @@
-pacstrap /mnt base linux linux-firmware base-devel dhcpcd networkmanager openssh vim intel-ucode git wget cmake lm_sensors i3 i3status xcompmgr dunst mesa lib32-mesa xf86-video-amdgpu vulkan-radeon libva-mesa-driver lib32-libva-mesa-driver mesa-vdpau lib32-mesa-vdpau acpi_call man-db neofetch htop
+pacstrap /mnt 
+
+########## Base Install ##########
+base linux linux-firmware 
+
+########## Development ##########
+base-devel 
+git 
+cmake 
+
+########## Networking ##########
+dhcpcd networkmanager openssh 
+
+########## Text Editor  ##########
+vim 
+
+########## Utilities ##########
+wget 
+man-db
+neofetch
+htop 
+ 
+
+########## Drivers ##########
+##### CPU #####
+intel-ucode 
+##### GPU #####
+mesa 
+lib32-mesa 
+xf86-video-amdgpu
+vulkan-radeon
+libva-mesa-driver
+lib32-libva-mesa-driver
+mesa-vdpau
+lib32-mesa-vdpau
+acpi_call 
+
+########## Sensors ##########
+lm_sensors 
+
+########## Desktop Environment ##########
+i3 
+i3status 
+lightdm
+xcompmgr 
+dunst 
