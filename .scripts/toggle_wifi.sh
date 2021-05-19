92e6daadd7a33a268ed66b7e8b0928041fad2a97
DiegoRenner <diego.renner@hotmail.com>
2021-05-20 01:46:26 +0200

    added wifi toggling and enhanced brightness control to WM conf

--- .scripts/toggle_wifi.sh
+++ .scripts/toggle_wifi.sh
@@ -1,3 +1,14 @@
-nmcli radio wifi on
-nmcli radio wifi off
+#!/bin/bash
 
+status=$(nmcli radio wifi)
+
+if [ $status = "enabled" ] 
+then
+	nmcli radio wifi off
+else
+	nmcli radio wifi on
+fi
+
+status=$(nmcli radio wifi)
+
+echo "WiFi has been: $status"
