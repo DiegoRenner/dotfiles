6658f9d7815681612f32daeac2f5bad0663dcc58
DiegoRenner <diego.renner@hotmail.com>
2025-05-26 04:02:00 +0200

    Delete .scripts/new_screen2.sh
    removed unused screen and gpu scripts

--- .scripts/new_screen2.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output DP-0 --off --output DP-1 --off --output DP-2 --mode 3840x2160 --pos 0x0 --rotate normal --output DP-3 --off --output DP-4 --mode 1920x1080 --pos 3840x0 --scale 2x2 --rotate normal --output DP-5 --off --output DP-6 --off
