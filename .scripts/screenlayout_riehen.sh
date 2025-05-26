583feb97576512e2cb43a2c52fb7287bc1234f35
DiegoRenner <diego.renner@hotmail.com>
2025-05-26 04:03:44 +0200

    Delete .scripts/screenlayout_riehen.sh
    removed unused screen and gpu scripts

--- .scripts/screenlayout_riehen.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-
-xrandr --output DP-0 --off --output DP-1 --mode 1680x1050 --pos 0x0 --scale 2x2 --rotate normal --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-5 --off --output eDP-1-1 --mode 3840x2160 --pos 3360x0 --rotate normal
