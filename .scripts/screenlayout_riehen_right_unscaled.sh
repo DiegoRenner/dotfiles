bdc45b024eb386f64190b10a83b61a458d1e15bb
DiegoRenner <diego.renner@hotmail.com>
2025-05-26 04:05:02 +0200

    Delete .scripts/screenlayout_riehen_right_unscaled.sh
    removed unused screen and gpu scripts

--- .scripts/screenlayout_riehen_right_unscaled.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output DP-0 --off --output DP-1 --mode 1680x1050 --pos 3840x0 --scale 1x1 --rotate normal --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-5 --off --output eDP-1-1 --primary --mode 3840x2160 --pos 0x0 --rotate normal
