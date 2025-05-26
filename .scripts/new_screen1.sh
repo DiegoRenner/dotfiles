8fdac85769739a31ceb39634f41ef2014ef8d28a
DiegoRenner <diego.renner@hotmail.com>
2025-05-26 04:01:24 +0200

    Delete .scripts/new_screen1.sh
    removed old screen and gpu scripts

--- .scripts/new_screen1.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-xrandr --output DP-0 --off --output DP-1 --mode 1680x1050 --scale 2x2 --pos 6000x0 --rotate normal --output DP-2 --mode 3840x2160 --pos 2160x0 --rotate normal --output DP-3 --off --output DP-4 --mode 1920x1080 --scale 2x2 --pos 0x0 --rotate left --output DP-5 --off --output DP-6 --off
-feh --bg-scale Pictures/wallpaper.jpg Pictures/wallpaper.jpg Pictures/wallpaper_portrait.jpg
