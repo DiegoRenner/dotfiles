80daea40894a39455c2fcb5ea0b457f045a20d37
DiegoRenner <diego.renner@hotmail.com>
2026-02-05 14:48:44 +0000

    feat: improved vimtex config

--- private_dot_config/nvim/lua/plugins/vimtex.lua
+++ private_dot_config/nvim/lua/plugins/vimtex.lua
@@ -2,29 +2,34 @@ return {
   {
     "lervag/vimtex",
     lazy = false,
-    config = function()
-      -- Disable conflicting latexmk viewer
+    init = function()
+      -- Use init() to set globals before the plugin loads
+
+      -- 1. LaTeXmk Configuration
       vim.g.vimtex_compiler_latexmk = {
         build_dir = "",
+        aux_dir = "",
+        out_dir = "",
         callback = 1,
         continuous = 1,
         executable = "latexmk",
         options = {
-          "-pdf",
-          "-interaction=nonstopmode",
+          "-verbose",
+          "-file-line-error",
           "-synctex=1",
-          "-shell-escape", -- <<< add this
-          "-view=none", -- important: let VimTeX manage the viewer
+          "-interaction=nonstopmode",
+          "-shell-escape",
+          "-view=none",
         },
       }
 
-      -- Viewer (Arch friendly: use zathura if installed, otherwise evince)
+      -- 2. Viewer Configuration (Zathura is best for Linux)
       vim.g.vimtex_view_method = "zathura"
       -- vim.g.vimtex_view_general_viewer = "xdg-open"
       vim.g.vimtex_view_forward_search_on_start = 1
+
+      -- 3. Prevent forward search from stealing focus every time you save
+      vim.g.vimtex_view_forward_search_on_start = 0
     end,
-    keys = {
-      { "<localLeader>l", "", desc = "+vimtex", ft = "tex" },
-    },
   },
 }
