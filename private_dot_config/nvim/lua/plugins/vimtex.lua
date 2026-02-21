e3ed82ce7ffed76affed6c85ea4281be9df707d1
DiegoRenner <diego.renner@hotmail.com>
2026-02-21 18:15:31 +0000

    fix: clean up vimtex config

--- private_dot_config/nvim/lua/plugins/vimtex.lua
+++ private_dot_config/nvim/lua/plugins/vimtex.lua
@@ -2,12 +2,13 @@ return {
   {
     "lervag/vimtex",
     lazy = false,
-    config = function()
-      -- Use init() to set globals before the plugin loads
+    init = function() -- MUST be init(), not config()
+      -- 1. Viewer Configuration
+      vim.g.vimtex_view_method = "zathura"
+      vim.g.vimtex_view_forward_search_on_start = 1
 
-      -- 1. LaTeXmk Configuration
+      -- 2. LaTeXmk Configuration
       vim.g.vimtex_compiler_latexmk = {
-        build_dir = "",
         aux_dir = "",
         out_dir = "",
         callback = 1,
@@ -15,21 +16,16 @@ return {
         executable = "latexmk",
         options = {
           "-pdf",
-          "-interaction=nonstopmode",
-          "-synctex=1",
           "-shell-escape",
-          "-view=none",
+          "-verbose",
+          "-file-line-error",
+          "-synctex=1",
+          "-interaction=nonstopmode",
         },
       }
 
-      -- 2. Viewer Configuration (Zathura is best for Linux)
-      vim.g.vimtex_view_method = "zathura"
-
-      -- 3. Prevent forward search from stealing focus every time you save
-      vim.g.vimtex_view_forward_search_on_start = 1
-
-      -- 4. BibLaTeX support
-      vim.g.vimtex_quickfix_mode = 0 -- Optional: suppress quickfix window popping up on warnings
+      -- 3. BibLaTeX support
+      vim.g.vimtex_quickfix_mode = 0
     end,
     keys = {
       { "<localLeader>l", "", desc = "+vimtex", ft = "tex" },
