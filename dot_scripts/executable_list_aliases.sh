599144b12898b8001543e0c7d8078d6691a6b3cc
DiegoRenner <diego.renner@hotmail.com>
2026-03-02 22:42:08 +0000

    feat: executable alias menu

--- dot_scripts/executable_list_aliases.sh
+++ dot_scripts/executable_list_aliases.sh
@@ -1,2 +1,6 @@
-#!/bin/fish
-alias | awk -F'[ =]' '{print $2}'
+#!/bin/bash
+# 1. Aliases from Fish
+fish -c "alias" | awk -F"[ =]" "{print \$2}"
+# 2. Applications from Desktop files
+ls /usr/share/applications/ | grep .desktop | xargs -I {} grep -h "^Exec=" /usr/share/applications/{} | cut -d"=" -f2 | cut -d" " -f1 | sed "s/%.*//" | xargs -n1 basename | sort -u
+
