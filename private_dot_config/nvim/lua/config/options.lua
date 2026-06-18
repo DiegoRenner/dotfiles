0185b0705f9f0e0de4cf15957918f594827bd1c9
DiegoRenner <diego.renner@hotmail.com>
2026-06-18 02:53:09 +0100

    feat(nvim): remove chezmoi highlighting in nvim to prevent reformatting

--- private_dot_config/nvim/lua/config/options.lua
+++ private_dot_config/nvim/lua/config/options.lua
@@ -9,18 +9,18 @@
 vim.g.lazyvim_rust_diagnostics = "rust-analyzer"
 
 -- Fix syntax highlighting for chezmoi files
-vim.filetype.add({
-  extension = {
-    tmpl = function(path, bufnr)
-      local new_path = path:gsub("%.tmpl$", "")
-      return vim.filetype.match({ filename = new_path })
-    end,
-  },
-  pattern = {
-    [".*/.local/share/chezmoi/.*"] = function(path, bufnr)
-      local name = vim.fn.fnamemodify(path, ":t")
-      name = name:gsub("^dot_", "."):gsub("^private_dot_", "."):gsub("^private_", ""):gsub("%.tmpl$", "")
-      return vim.filetype.match({ filename = name })
-    end,
-  },
-})
+-- vim.filetype.add({
+--   extension = {
+--     tmpl = function(path, bufnr)
+--       local new_path = path:gsub("%.tmpl$", "")
+--       return vim.filetype.match({ filename = new_path })
+--     end,
+--   },
+-- pattern = {
+--   [".*/.local/share/chezmoi/.*"] = function(path, bufnr)
+--     local name = vim.fn.fnamemodify(path, ":t")
+--     name = name:gsub("^dot_", "."):gsub("^private_dot_", "."):gsub("^private_", "")
+--     return vim.filetype.match({ filename = name })
+--   end,
+-- },
+-- })
